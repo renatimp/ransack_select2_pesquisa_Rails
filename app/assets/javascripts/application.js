2ec765b729046de684042a80ff3d9b10e1b718fc
Renato Marques <renatomarques.contato@gmail.com>
2021-02-18 13:31:27 +0000

    First Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require select2
+//= require_tree .
+
+$(document).ready(function() {
+    $( "#q_name_eq" ).select2();
+ });
